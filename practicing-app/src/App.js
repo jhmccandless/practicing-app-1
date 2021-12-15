052e0951bd2fec12ce9d4d8c765841d086300f3c
Jason McCandless <jhmccandless1105@gmail.com>
2021-12-14 23:10:29 -0600

    working on a timer

--- practicing-app/src/App.js
+++ practicing-app/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
-import { Comp1 } from "./components/Comp1";
+import Comp1 from "./components/Comp1";
 import "./App.css";
 
 function App() {
--- practicing-app/src/components/Comp1.jsx
+++ practicing-app/src/components/Comp1.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
-export function Comp1() {
-  return <h1>This is a comp!</h1>;
+function Comp1() {
+  return (
+    <>
+      <h1>This is a comp!</h1>
+      <div></div>
+    </>
+  );
 }
+
+export default Comp1;
