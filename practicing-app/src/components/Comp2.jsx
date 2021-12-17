f95a6e831e807ca4aa7dba2c953f13d193535c4b
Jason McCandless <jhmccandless1105@gmail.com>
2021-12-16 23:37:56 -0600

    have some sort of timer working

--- practicing-app/src/components/Comp2.jsx
+++ practicing-app/src/components/Comp2.jsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react";
 
 function Comp2() {
-  const [timer, setTimer] = useState(10);
+  const time = new Date();
+  const newTime = Date.now();
+  console.log(time);
+  setInterval(() => {
+    // console.log(Date.now() - newTime);
+  }, 1000);
   return (
     <>
       <h2>This is comp2</h2>
-      <div>this is the timer: {timer}</div>
+      <div>this is the timer</div>
     </>
   );
 }
