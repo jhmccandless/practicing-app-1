64132b2c0f0afd0f1cd59c406facb95c36dd8ec5
Jason McCandless <jhmccandless1105@gmail.com>
2021-12-18 12:20:07 -0600

    working on timer still

--- practicing-app/src/components/Comp2.jsx
+++ practicing-app/src/components/Comp2.jsx
@@ -1,16 +1,40 @@
 import React, { useState, useEffect } from "react";
 
 function Comp2() {
-  const time = new Date();
-  const newTime = Date.now();
-  console.log(time);
-  setInterval(() => {
-    // console.log(Date.now() - newTime);
-  }, 1000);
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  function toggle() {
+    setIsActive(!isActive);
+  }
+
+  function reset() {
+    setSeconds(0);
+    setIsActive(false);
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds((seconds) => seconds + 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
   return (
     <>
       <h2>This is comp2</h2>
-      <div>this is the timer</div>
+      <div>this is the timer: {seconds}</div>
+      <button
+        onClick={() => {
+          return isActive ? setIsActive(false) : setIsActive(true);
+        }}
+      >
+        Start/Stop Timer
+      </button>
     </>
   );
 }
